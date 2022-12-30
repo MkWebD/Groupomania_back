facc7246f2e2946b11e40b6e2ef30e9a117b82c5
Kevin Mas <kmas@thess-corp.fr>
2022-12-30 16:29:40 +0100

    fix: modifications for mongoDB

--- routes/post.js
+++ routes/post.js
@@ -3,21 +3,21 @@ const express = require("express");
 
 // Importing middlewares
 const multer = require("../middleware/multer-config");
-const auth = require("../middleware/authentification")
+const auth = require("../middleware/authentification");
 
 // Importing methods for post
-const {createPost} = require("../controllers/post/create");
-const {readPost, readOnePost} = require("../controllers/post/read");
-const {updatePost} = require("../controllers/post/update");
-const {deletePost} = require("../controllers/post/delete");
-const {likePost} = require("../controllers/post/like");
+const { createPost } = require("../controllers/post/create");
+const { readPost, readOnePost } = require("../controllers/post/read");
+const { updatePost } = require("../controllers/post/update");
+const { deletePost } = require("../controllers/post/delete");
+const { likePost } = require("../controllers/post/like");
 
 // Creating express Router
 const router = express.Router();
 
 // Routing for sauces
 router.post("/", auth, multer, createPost);
-router.get("/",auth, readPost);
+router.get("/", auth, readPost);
 router.get("/:id", auth, readOnePost);
 router.put("/:id", auth, multer, updatePost);
 router.delete("/:id", auth, multer, deletePost);
